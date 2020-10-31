af0c6eaab8f05cf9205691cc9068777db0b8198b
datomegasherly <dat.omega.sherly@gmail.com>
2020-10-31 22:05:44 +0330

    update App component to add bootstrap css, City component and base design of App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import City from './City';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
     render(){
         return (
-            <div data-test="app-component">
-
+            <div data-test="app-component" className="container-fluid">
+                <div className="row">
+                    <div className="col-12 text-center mt-3">
+                        <h1>Weather Box</h1>
+                    </div>
+                    <div className="col-12">
+                        <City />
+                    </div>
+                </div>
             </div>
         )
     }
