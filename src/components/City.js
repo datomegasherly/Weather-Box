0f47eaa0d959e606ca208bda500f5793830a9486
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-01 11:14:42 +0330

    add redux connect with mapState and selectCity action to basic update selectedCities state

--- src/components/City.js
+++ src/components/City.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectCity } from '../actions';
 
 class City extends Component {
+    addCity = (ev) => {
+        const { selectCity } = this.props;
+        if(ev.keyCode && ev.keyCode == 13){
+            let value = ev.target.value;
+            selectCity(value);
+        }
+    }
     render() {
+        const { selectedCities } = this.props;
         return (
             <div data-test="city-component">
-                
+                <input type="text" onKeyUp={this.addCity} />
             </div>
         )
     }
 }
 
-export default City;
+const mapStateToProps = state => {
+    const { selectedCities } = state;
+    return { selectedCities };
+};
+
+export default connect(mapStateToProps, { selectCity })(City);
