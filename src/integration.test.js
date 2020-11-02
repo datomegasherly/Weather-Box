3a2e8c11e6c81df52568d9e71ce8d3bca62c1a49
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-02 10:09:05 +0330

    redux reducer and action tests together for selectedCities update

---
+++ src/integration.test.js
@@ -0,0 +1,20 @@
+import { storeFactory } from '../test/testUtils';
+import { selectCity } from './actions';
+
+describe('selectCity action dispatch', () => {
+    it('no city selected yet', () => {
+        const initialState = { selectedCities: [] };
+        let store = storeFactory(initialState);
+        store.dispatch(selectCity('city1'));
+        const newState = store.getState();
+        expect(newState).toEqual({ selectedCities: ['city1'] });
+    });
+    it('some cities selected before', () => {
+        const initialState = { selectedCities: [] };
+        let store = storeFactory(initialState);
+        store.dispatch(selectCity('city1'));
+        store.dispatch(selectCity('city2'));
+        const newState = store.getState();
+        expect(newState).toEqual({ selectedCities: ['city1','city2'] });
+    });
+});
