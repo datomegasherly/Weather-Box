1a53bec5dd08f7d2477e394fd06873dc20562e14
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-02 17:17:20 +0330

    add isFocus state to show on focus and hide on blur of select city input , add city-box to show city search results and select cities

--- src/components/City.js
+++ src/components/City.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { selectCity } from '../actions';
+import './City.css';
 
 class City extends Component {
+    /**
+     * @param isFocus - It shows that select city input is focus or blur to show or hide city-box
+     */
+    state = {
+        isFocus: false
+    }
     /**
      * @function addCity add City to redux state "selectedCities"
      * @param {Object} ev - Javascript Event ( KeyUp event )
      */
-    addCity = (ev) => {
+    updateCityBox = (ev) => {
         const { selectCity } = this.props;
         if(ev.keyCode && ev.keyCode == 13){ // check if keyup is enter ( code : 13 )
             let value = ev.target.value;
             selectCity(value);
         }
     }
+
     render() {
+        let { isFocus } = this.state;
         return (
             <div className="container mt-4" data-test="city-component">
                 <div 
@@ -31,8 +40,17 @@ class City extends Component {
                         data-test="select-city-input"
                         type="text"
                         placeholder="type City then Enter"
-                        onKeyUp={this.addCity}
+                        onKeyUp={this.updateCityBox}
+                        onFocus={() => this.setState({isFocus: true})}
+                        onBlur={() => setTimeout(() => this.setState({isFocus: false}), 50)} // use setTimeout to let user click on a city from city-box to select before hide city-box
                     />
+                    {
+                        isFocus ? 
+                        <div className="city-box">
+                            
+                        </div>
+                        : ''
+                    }
                 </div>
             </div>
         )
@@ -45,8 +63,8 @@ City.propTypes = {
 };
 
 const mapStateToProps = state => {
-    const { selectedCities } = state;
-    return { selectedCities };
+    const { cities, selectedCities } = state;
+    return { cities, selectedCities };
 };
 
 export default connect(mapStateToProps, { selectCity })(City);
