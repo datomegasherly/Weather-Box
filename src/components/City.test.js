f73a87b56e707df09d481fd6b247722b3ff0967a
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-03 13:40:04 +0330

    add citysearch state testes and add isFocus testing for city box

--- src/components/City.test.js
+++ src/components/City.test.js
@@ -4,9 +4,11 @@ import { getByAttr, checkPropType, storeFactory } from '../../test/testUtils';
 
 import City from './City';
 
-const setup = (state) => {
-    const store = storeFactory(state);
-    return shallow(<City store={store} />).dive().dive();
+const setup = (initialState = {}, internalState) => {
+    const store = storeFactory(initialState);
+    const wrapper = shallow(<City store={store} />).dive().dive();
+    if(internalState) wrapper.setState(internalState);
+    return wrapper;
 }
 
 it('City component render test', () => {
@@ -15,10 +17,41 @@ it('City component render test', () => {
     expect(component.length).toBe(1);
 });
 
-it('render select city Input', () => {
+describe('render select city', () => {
     const wrapper = setup();
     const selectCityInput = getByAttr(wrapper, 'select-city-input');
-    expect(selectCityInput.length).toBe(1);
+    it('render select city Input', () => {
+        expect(selectCityInput.length).toBe(1);
+    })
+    describe('citySearch state test', () => {
+        it('`citySearch` is empty when City component loaded', () => {
+            const internalState = wrapper.state('citySearch');
+            expect(internalState).toBe('');
+        })
+        it('update `citySearch` state on change select city input', () => {
+            selectCityInput.simulate('change', {target: {value: 'test'}});
+            const citySearch = wrapper.state('citySearch');
+            expect(citySearch).toBe('test');
+        })
+    })
+})
+
+describe('render city box', () => {
+    it('`isFocus` is `false` when City component loaded', () => {
+        const wrapper = setup();
+        const internalState = wrapper.state('isFocus');
+        expect(internalState).toBe(false);
+    })
+    it('do not render city box when `isFocus` state is `false`', () => {
+        const wrapper = setup(null, {isFocus: false});
+        const cityBox = getByAttr(wrapper, 'city-box');
+        expect(cityBox.length).toBe(0);
+    })
+    it('render city box when `isFocus` state is `true`', () => {
+        const wrapper = setup(null, {isFocus: true});
+        const cityBox = getByAttr(wrapper, 'city-box');
+        expect(cityBox.length).toBe(1);
+    })
 })
 
 it('Check selectCity prop to be function', () => {
