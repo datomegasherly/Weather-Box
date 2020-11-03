f8b99276f7a1ceea916abd5e37db367135362dc0
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-03 15:42:40 +0330

    some changes in integration test becuase of change selectedCities variables from string to object {id, name}

--- src/integration.test.js
+++ src/integration.test.js
@@ -5,16 +5,16 @@ describe('selectCity action dispatch', () => {
     it('no city selected yet', () => {
         const initialState = { selectedCities: [] };
         let store = storeFactory(initialState);
-        store.dispatch(selectCity('city1'));
+        store.dispatch(selectCity({id:1, name:'test'}));
         const newState = store.getState();
-        expect(newState.selectedCities).toEqual(['city1']);
+        expect(newState.selectedCities).toEqual([{id:1, name:'test'}]);
     });
     it('some cities selected before', () => {
         const initialState = { selectedCities: [] };
         let store = storeFactory(initialState);
-        store.dispatch(selectCity('city1'));
-        store.dispatch(selectCity('city2'));
+        store.dispatch(selectCity({id:1, name:'test'}));
+        store.dispatch(selectCity({id:2, name:'test2'}));
         const newState = store.getState();
-        expect(newState.selectedCities).toEqual(['city1','city2']);
+        expect(newState.selectedCities).toEqual([{id:1, name:'test'},{id:2, name:'test2'}]);
     });
 });
