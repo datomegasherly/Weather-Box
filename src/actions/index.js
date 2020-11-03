0a316ba324042e3e7aef5e05c0b01620ab2593d9
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-03 15:41:34 +0330

    add check propTypes to selectCity function and update test with new variables

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,24 @@
 import actionTypes from '../actionTypes';
+import PropTypes from 'prop-types';
 /**
  * @function selectCity will add a city to selectedCity reducer
  * @param {string} city 
  * @returns {object}
  */
-export const selectCity = city => {
+const selectCity = city => {
     return {
         type: actionTypes.SET_CITY,
         payload: city
     }
 }
+
+selectCity.propTypes = {
+    city: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired
+    })
+}
+
+export {
+    selectCity
+};
--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -2,8 +2,8 @@ import { selectCity } from './';
 import actionTypes from '../actionTypes';
 
 describe('selectCity', () => {
-    it('return correct object when city:`test` send to function', () => {
-        const func = selectCity('test');
-        expect(func).toEqual({type: actionTypes.SET_CITY, payload: 'test'});
+    it('return correct object when city:`{id:1, name:`test`}` send to function', () => {
+        const func = selectCity({id: 1,name: 'test'});
+        expect(func).toEqual({type: actionTypes.SET_CITY, payload: {id: 1, name: 'test'}});
     });
 })
