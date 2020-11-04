604f0528480342f425297d60ce7fd3e65f764c64
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-04 12:30:43 +0330

    update weather to show weather-box on every city added to selectedCities

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import './Weather.css';
 
 class Weather extends Component {
     render() {
+        const { selectedCities } = this.props;
         return (
-            <div data-test="weather-component">
-                
+            <div className="container-fluid mt-3" data-test="weather-component">
+                {
+                    selectedCities && selectedCities.length ? 
+                        <div data-test="weather-box" className="row weather-box">
+                        {
+                            selectedCities.map(r => {
+                                return (
+                                    <div key={Math.round(Math.random()*10000000)} className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                                        <div className="weather-box-inner">
+                                            {r.name}
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                        </div>
+                    : ''
+                }
             </div>
         )
     }
 }
 
-export default Weather;
+Weather.propTypes = {
+    selectedCities: PropTypes.array.isRequired
+}
+
+const mapStateToProps = state => {
+    const { selectedCities } = state;
+    return { selectedCities };
+}
+
+export default connect(mapStateToProps)(Weather);
