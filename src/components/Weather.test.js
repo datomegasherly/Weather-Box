30a829483b90865119cc65a0318d805a36e90da3
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-04 12:32:04 +0330

    add two test to check open weather-box on city selected and check selectedCities prop on update from City component

--- src/components/Weather.test.js
+++ src/components/Weather.test.js
@@ -1,15 +1,37 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import { getByAttr } from '../../test/testUtils';
+import { getByAttr, storeFactory } from '../../test/testUtils';
+import { selectCity } from '../actions';
 
 import Weather from './Weather';
 
-const setup = (props = {}) => {
-    return shallow(<Weather {...props} />);
+const setup = (store = {}) => {
+    const wrapper = shallow(<Weather store={store} />).dive().dive();
+    return wrapper;
 }
 
 it('Weather component render test', () => {
-    const wrapper = setup();
+    const state = { selectedCities: [] };
+    let store = storeFactory(state);
+    const wrapper = setup(store);
     const component = getByAttr(wrapper, 'weather-component');
     expect(component.length).toBe(1);
+});
+
+it('Check open weather-box when a test city is selected', () => {
+    const state = { selectedCities: [] };
+    let store = storeFactory(state);
+    store.dispatch(selectCity({id: 1, name: 'test'}));
+    const wrapper = setup(store);
+    const weatherBox = getByAttr(wrapper, 'weather-box');
+    expect(weatherBox.length).toBe(1);
+});
+
+it('Check `selectedCities` prop type to be array', () => {
+    const state = { selectedCities: [] };
+    let store = storeFactory(state);
+    store.dispatch(selectCity({id: 1, name: 'test'}));
+    const wrapper = setup(store);
+    const weatherStore = store.getState();
+    expect(weatherStore.selectedCities).toEqual([{id: 1, name: 'test'}]);
 });
