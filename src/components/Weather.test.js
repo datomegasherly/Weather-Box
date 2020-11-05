71b723a3c230ee33de5a2ad389e44d346f90bf19
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-05 22:19:27 +0330

    update weather tests with moxios and new selectCity action

--- src/components/Weather.test.js
+++ src/components/Weather.test.js
@@ -2,6 +2,8 @@ import { shallow } from 'enzyme';
 import React from 'react';
 import { getByAttr, storeFactory } from '../../test/testUtils';
 import { selectCity } from '../actions';
+import moxios from 'moxios';
+import { cityData } from '../helpers';
 
 import Weather from './Weather';
 
@@ -18,20 +20,36 @@ it('Weather component render test', () => {
     expect(component.length).toBe(1);
 });
 
-it('Check open weather-box when a test city is selected', () => {
-    const state = { selectedCities: [] };
-    let store = storeFactory(state);
-    store.dispatch(selectCity({id: 1, name: 'test'}));
-    const wrapper = setup(store);
-    const weatherBox = getByAttr(wrapper, 'weather-box');
-    expect(weatherBox.length).toBe(1);
-});
-
-it('Check `selectedCities` prop type to be array', () => {
-    const state = { selectedCities: [] };
-    let store = storeFactory(state);
-    store.dispatch(selectCity({id: 1, name: 'test'}));
-    const wrapper = setup(store);
-    const weatherStore = store.getState();
-    expect(weatherStore.selectedCities).toEqual([{id: 1, name: 'test'}]);
+describe('Async Test with moxios', () => {
+    beforeEach(() => {
+        moxios.install();
+        moxios.wait(() => {
+            const request = moxios.requests.mostRecent();
+            request.respondWith({
+                status: 200,
+                response: cityData
+            });
+        });
+    })
+    afterEach(() => {
+        moxios.uninstall();
+    })
+    it('Check open weather-box when a test city is selected', () => {
+        const state = { selectedCities: [] };
+        let store = storeFactory(state);
+        return store.dispatch(selectCity({id: 6942553, name: 'Paris'})).then(() => {
+            const wrapper = setup(store);
+            const weatherBox = getByAttr(wrapper, 'weather-box');
+            expect(weatherBox.length).toBe(1);
+        });
+    });
+    
+    it('Check `selectedCities` prop type to be array', () => {
+        const state = { selectedCities: [] };
+        let store = storeFactory(state);
+        return store.dispatch(selectCity({id: 6942553, name: 'Paris'})).then(() => {
+            const weatherStore = store.getState();
+            expect(weatherStore.selectedCities).toStrictEqual([{id: 6942553, name: 'Paris', data: cityData}]);
+        });
+    });
 });
