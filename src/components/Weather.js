4a4447bcc998e84bfef3c3414a60fc0cfa9a9bf8
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-06 02:18:31 +0330

    countryflags.io api uses to show country flag

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import './Weather.css';
-
+/**
+ * countryflags.io api uses to show country flag
+ */
 class Weather extends Component {
     render() {
         const { selectedCities } = this.props;
@@ -13,10 +15,11 @@ class Weather extends Component {
                         <div data-test="weather-box" className="row weather-box">
                         {
                             selectedCities.map(r => {
+                                let { data } = r;
                                 return (
                                     <div key={r.id} className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                                         <div className="weather-box-inner">
-                                            {r.name}
+                                            <div>{data.sys && data.sys.country && <img src={`https://www.countryflags.io/${data.sys.country}/shiny/32.png`} />} {r.name}</div>
                                         </div>
                                     </div>
                                 )
