9e1944af6cead7637c66ae963f055a49a6188b62
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-06 02:08:50 +0330

    update selectedCities map div key from Math.random to city id , because City component prevent duplicate city selection

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -14,7 +14,7 @@ class Weather extends Component {
                         {
                             selectedCities.map(r => {
                                 return (
-                                    <div key={Math.round(Math.random()*10000000)} className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                                    <div key={r.id} className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                                         <div className="weather-box-inner">
                                             {r.name}
                                         </div>
