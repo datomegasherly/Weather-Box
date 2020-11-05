3b1c2d34681fc4d2ea27193c86b09b951d328b1c
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-05 21:33:14 +0330

    add @babel/polyfill to enzyme setup to use async await in redux actions

--- enzyme.config.js
+++ enzyme.config.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill'; // add polyfill to enzyme config to use async/await in actions using redux thunk
 import { configure } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
