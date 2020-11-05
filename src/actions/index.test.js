8251b6916ff47d24fa7b40a6473780c44b760dc4
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-05 22:18:21 +0330

    update action test with moxios and new selectCity action

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,9 +1,28 @@
-import { selectCity } from './';
+import { selectCity, updateCity } from './';
 import actionTypes from '../actionTypes';
+import moxios from 'moxios';
+import { storeFactory } from '../../test/testUtils';
+import { cityData } from '../helpers';
 
-describe('selectCity', () => {
-    it('return correct object when city:`{id:1, name:`test`}` send to function', () => {
-        const func = selectCity({id: 1,name: 'test'});
-        expect(func).toEqual({type: actionTypes.SET_CITY, payload: {id: 1, name: 'test'}});
+describe('selectCity action creator', () => {
+    beforeEach(() => {
+        moxios.install(); // install moxios before each test
+        moxios.wait(() => {
+            const request = moxios.requests.mostRecent();
+            request.respondWith({
+                status: 200,
+                response: cityData
+            });
+        });
     });
-})
+    afterEach(() => {
+        moxios.uninstall(); // uninstall moxios after each test
+    });
+    it('send current city id ( and name ) and add data variable with weather data to current city', () => {
+        let store = storeFactory();
+        return store.dispatch(selectCity({id: 6942553, name: 'Paris'})).then(() => {
+            const newState = store.getState();
+            expect(newState.selectedCities).toStrictEqual([{id: 6942553, name: 'Paris', data: cityData}]);
+        });
+    });
+});
