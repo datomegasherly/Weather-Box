2f5e7dc74ffa04ba79ebba98767a038492358a0d
datomegasherly <dat.omega.sherly@gmail.com>
2020-12-19 14:40:57 +0330

    add triggerDetails to trigger in more details of weather , add hideDetails to set current city details to false when delete a city weather box, add showDetails state

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,15 +1,24 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
-import actionTypes from '../actionTypes';
 import { removeCity } from '../actions';
 import './Weather.css';
 /**
  * countryflags.io api uses to show country flag
  */
 class Weather extends Component {
+    state = {
+        showDetails: {}
+    }
+    triggerDetails(id) {
+        this.setState({showDetails: {...this.state.showDetails , [id]: !this.state.showDetails[id]}});
+    }
+    hideDetails(id) {
+        this.setState({showDetails: {...this.state.showDetails , [id]: false}});
+    }
     render() {
         const { selectedCities, removeCity } = this.props;
+        let { showDetails } = this.state;
         return (
             <div className="container-fluid mt-3" data-test="weather-component">
                 {
@@ -22,7 +31,11 @@ class Weather extends Component {
                                     let { sys, main } = data;
                                     return (
                                         <div key={city.id} className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-                                            <div className="float-right mr-2 ml-2 mt-1 close-button" data-test="close-button" onClick={() => removeCity(city)}>
+                                            <div className="float-right mr-2 ml-2 mt-1 close-button" data-test="close-button" 
+                                                onClick={() => {
+                                                    this.hideDetails(city.id);
+                                                    removeCity(city)
+                                                }}>
                                                 <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                                     <path fillRule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                                                 </svg>
@@ -32,7 +45,7 @@ class Weather extends Component {
                                                     weather ?
                                                         <Fragment>
                                                             <div className="row">
-                                                                <div className="col-12">
+                                                                <div className="col-12 weather-country-box">
                                                                     {sys && sys.country && <img src={`https://www.countryflags.io/${sys.country}/shiny/32.png`} />} {city.name}
                                                                 </div>
                                                                 <div className="col-12">
@@ -59,20 +72,23 @@ class Weather extends Component {
                                                                 </div>
                                                             </div>
                                                             <div className="col-12 row weather-option-box">
-                                                                <div className="col-12">Latest Weather Conditions</div>
+                                                                <div className="col-12" onClick={() => this.triggerDetails(city.id)}>Latest Weather Conditions</div>
                                                             </div>
-                                                            <div className="col-12 row">
-                                                                <div className="col-6 mb-1">Temperature :</div>
-                                                                <div className="col-6 mb-1">{Math.round(main.temp)} &#8451;</div>
-                                                                <div className="col-6 mb-1">Real Feel :</div>
-                                                                <div className="col-6 mb-1">{Math.round(main.feels_like)} &#8451;</div>
-                                                                <div className="col-6 mb-1">Min Temp. :</div>
-                                                                <div className="col-6 mb-1">{Math.round(main.temp_min)} &#8451;</div>
-                                                                <div className="col-6 mb-1">Max Temp. :</div>
-                                                                <div className="col-6 mb-1">{Math.round(main.temp_max)} &#8451;</div>
-                                                                <div className="col-6">humidity :</div>
-                                                                <div className="col-6">{Math.round(main.humidity)} %</div>
-                                                            </div>
+                                                            {
+                                                                showDetails[city.id] ?
+                                                                    <div className="col-12 row">
+                                                                        <div className="col-6 mb-1">Temperature :</div>
+                                                                        <div className="col-6 mb-1">{Math.round(main.temp)} &#8451;</div>
+                                                                        <div className="col-6 mb-1">Real Feel :</div>
+                                                                        <div className="col-6 mb-1">{Math.round(main.feels_like)} &#8451;</div>
+                                                                        <div className="col-6 mb-1">Min Temp. :</div>
+                                                                        <div className="col-6 mb-1">{Math.round(main.temp_min)} &#8451;</div>
+                                                                        <div className="col-6 mb-1">Max Temp. :</div>
+                                                                        <div className="col-6 mb-1">{Math.round(main.temp_max)} &#8451;</div>
+                                                                        <div className="col-6">humidity :</div>
+                                                                        <div className="col-6">{Math.round(main.humidity)} %</div>
+                                                                    </div> : ''
+                                                            }
                                                         </Fragment>
                                                         : ''
                                                 }
